e838a6e73e3f7ad98dfc1e27e8b602c9a25f36f0
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-09-21 20:55:06 +0900

    testcode RED (Login API)

---
+++ src/tests/Feature/LoginApiTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class LoginApiTest extends TestCase
+{
+  use RefreshDatabase;
+
+  public function setUp(): void
+  {
+    parent::setUp();
+    $this->user = factory(User::class)->create();
+  }
+
+  /**
+   * @test
+   */
+  public function 正常系()
+  {
+    $data = [
+      'email' => $this->user->email,
+      'password' => 'password',
+    ];
+
+    $response = $this->json('POST', route('login'), $data);
+
+    $response->assertStatus(200);
+    $this->assertEquals($this->user->account_id, $response['account_id']);
+    $this->assertEquals($this->user->name, $response['name']);
+  }
+
+  /**
+   * @test
+   */
+  public function 不正系_不正なemail、password()
+  {
+    $data = [
+      'email' => '',
+      'password' => '',
+    ];
+
+    $response = $this->json('POST', route('login'), $data);
+
+    $response->assertStatus(422);
+  }
+  
+  /**
+   * @test
+   */
+  public function 不正系_照合が合わない()
+  {
+    $data = [
+      'email' => $this->user->email,
+      'password' => 'pass',
+    ];
+  
+    $response = $this->json('POST', route('login'), $data);
+  
+    $response->assertStatus(422);    
+  }
+}
