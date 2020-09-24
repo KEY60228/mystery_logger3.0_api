4808ba5ffe4f11c8b7c01f86c153bcbf6058ff19
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-09-24 19:31:54 +0900

    migrate products table

---
+++ src/database/SQL/insert_products_table.sql
@@ -0,0 +1,8 @@
+INSERT INTO products (name, contents, image_name, created_at, updated_at) VALUES ('魔王城からの脱出', '魔王は倒した、だけど城から出られない！？', 'demon_castle.jpg', now(), now());
+INSERT INTO products (name, contents, image_name, created_at, updated_at) VALUES ('魔王城からの脱出Ⅱ', '繰り返す世界崩壊のループを攻略せよ！！', 'demon_castle2.jpg', now(), now());
+INSERT INTO products (name, contents, image_name, created_at, updated_at) VALUES ('夜のミステリーホテルからの脱出', '全ての謎を解いたとき、あなたは自分が何者かを知る。', 'night_mystery_hotel.jpg', now(), now());
+INSERT INTO products (name, contents, image_name, created_at, updated_at) VALUES ('あるラジオ放送局からの脱出', '伝説の番組を放送できるか！？', 'radio_studio.jpg', now(), now());
+INSERT INTO products (name, contents, image_name, created_at, updated_at) VALUES ('閉ざされた雪山からの脱出', 'この極限を、生き延びろ。', 'snowy_mountain.jpg', now(), now());
+INSERT INTO products (name, contents, image_name, created_at, updated_at) VALUES ('ある沈没戦からの脱出', '酸素、残り10分', 'sunken_ship.jpg', now(), now());
+INSERT INTO products (name, contents, image_name, created_at, updated_at) VALUES ('ヴァンパイアホテルからの脱出', 'この部屋から1人では出られない', 'vampire_hotel.jpg', now(), now());
+INSERT INTO products (name, contents, image_name, created_at, updated_at) VALUES ('不思議の国からの脱出', 'あなたが迷い込んだのは、謎に彩られたアリスの世界。', 'wonderland.jpg', now(), now());
