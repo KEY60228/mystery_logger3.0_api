d2770c15a0c83487be54d92972b8e223c0d78ac7
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-09-24 19:30:30 +0900

    testcode RED (GetAllProducts API)

---
+++ src/tests/Feature/GetAllProductsApiTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Product;
+use Illuminate\Support\Facades\Response;
+
+class GetAllProductsApiTest extends TestCase
+{
+  use RefreshDatabase;
+
+  public function setUp(): void
+  {
+    parent::setUp();
+    $this->product = factory(Product::class)->create();
+  }
+
+  /**
+   * @test
+   */
+  public function 正常系()
+  {
+    $response = $this->json('GET', route('products'));
+
+    $response->assertStatus(200)->assertJsonFragment([
+      'name' => $this->product->name,
+      'contents' => $this->product->contents,
+      'image_name' => $this->product->image_name,
+    ]);
+  }
+}
