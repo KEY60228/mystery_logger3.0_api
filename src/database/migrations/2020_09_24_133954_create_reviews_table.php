3f121c7ddf6544826a543d3ac5328ae310c09e82
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-09-25 20:37:35 +0900

    migrate reviews table

---
+++ src/database/migrations/2020_09_24_133954_create_reviews_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('product_id');
+            $table->string('contents');
+            $table->integer('result');
+            $table->integer('clear_time')->nullable();
+            $table->float('rating', 3, 1)->nullable();
+            $table->date('joined_at')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
