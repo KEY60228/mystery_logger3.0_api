ed66ae11c03c20d363b848950e0d853cb0963c54
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-09-26 18:15:09 +0900

    testcode RED (modify GetProductDetail API)

--- src/tests/Feature/GetProductDetailApi.php
+++ src/tests/Feature/GetProductDetailApi.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Product;
+use App\Models\User;
+use App\Models\Review;
 
 class GetProductDetailApi extends TestCase
 {
@@ -15,6 +17,11 @@ class GetProductDetailApi extends TestCase
   {
     parent::setUp();
     $this->product = factory(Product::class)->create();
+    $this->user = factory(User::class)->create();
+    $this->review = factory(Review::class, 3)->create([
+      'user_id' => $this->user->id,
+      'product_id' => $this->product->id,
+    ]);
   }
 
   /**
@@ -26,11 +33,13 @@ class GetProductDetailApi extends TestCase
       'id' => $this->product->id
     ]));
 
-    $response->assertStatus(200);
-
-    $response->assertStatus(200)->assertJsonFragment([
+    $response->assertStatus(200)->assertJson([
       'id' => $this->product->id,
       'name' => $this->product->name,
+      'reviews' => [[
+        'user_id' => $this->user->id,
+        'product_id' => $this->product->id,
+      ]]
     ]);
   }
 }
