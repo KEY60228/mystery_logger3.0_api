0a1041dfc32d2785be160e2cf61dddc933ed9205
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-09-28 15:26:17 +0900

    testcode RED (modify GetAllProducts API)

--- src/tests/Feature/Product/GetAllProductsApiTest.php
+++ src/tests/Feature/Product/GetAllProductsApiTest.php
@@ -6,7 +6,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Product;
-use Illuminate\Support\Facades\Response;
+use App\Models\User;
+use App\Models\Review;
 
 class GetAllProductsApiTest extends TestCase
 {
@@ -15,7 +16,12 @@ class GetAllProductsApiTest extends TestCase
   public function setUp(): void
   {
     parent::setUp();
+    $this->user = factory(User::class)->create();
     $this->product = factory(Product::class)->create();
+    $this->review = factory(Review::class, 3)->create([
+      'user_id' => $this->user->id,
+      'product_id' => $this->product->id,
+    ]);
   }
 
   /**
@@ -25,10 +31,24 @@ class GetAllProductsApiTest extends TestCase
   {
     $response = $this->json('GET', route('product.index'));
 
-    $response->assertStatus(200)->assertJsonFragment([
+    $reviewCount = Review::whereProductId($this->product->id)->count();
+    $avgRating = Review::whereProductId($this->product->id)->avg('rating');
+
+    $allCount = Review::where('product_id', $this->product->id)->count();
+    $successCount = Review::where('product_id', $this->product->id)->where('result', 1)->count();
+    $NACount = Review::where('product_id', $this->product->id)->where('result', 0)->count();
+    $successRate = $successCount / ($allCount - $NACount);
+
+    $response->assertStatus(200)->assertJson([[
+      'id' => $this->product->id,
       'name' => $this->product->name,
-      'contents' => $this->product->contents,
-      'image_name' => $this->product->image_name,
-    ]);
+      'reviews' => [[
+        'user_id' => $this->user->id,
+        'product_id' => $this->product->id,
+      ]],
+      'reviews_count' => $reviewCount,
+      'avgRating' => $avgRating,
+      'successRate' => $successRate,
+    ]]);
   }
 }
