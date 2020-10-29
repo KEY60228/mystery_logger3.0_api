ee5a1ddb7c06d849e2d20b7a5d7ece06955331a5
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-10-29 19:54:01 +0900

    testcode RED (Timeline API * TEMP)

---
+++ src/tests/Feature/Review/GetReviewsApiTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Product;
+use App\Models\Review;
+
+class GetReviewsApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = factory(User::class)->create();
+        $this->product = factory(Product::class)->create();
+        $this->reviews = factory(Review::class, 4)->create([
+            'user_id' => $this->user->id,
+            'product_id' => $this->product->id,
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function 正常系()
+    {
+        $response = $this->json('GET', route('review.timeline'), [
+            'user_id' => $this->user->id,
+        ]);
+
+        $response->assertStatus(200)->assertJson([[
+            'user_id' => $this->user->id,
+            'product_id' => $this->product->id,
+        ]]);
+    }
+}
