ce19bdd9eddd7b94be9ea4a7f4b420a31bbf465f
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-05-02 19:32:23 +0900

    change file system to S3 from file server #11

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -95,8 +95,10 @@ class UserController extends Controller
             }
             $filename .= '.' . $extension;
             
-            // ファイル保存
-            Storage::disk('public')->putFileAs('/user_img', $request->image_name, $filename);
+            // ファイル保存 (開発環境)
+            // Storage::disk('public')->putFileAs('/user_img', $request->image_name, $filename);
+            // ファイル保存 (S3環境)
+            Storage::disk('s3')->putFilesAs('/user_img', $request->image_name, $filename);
     
             // DBエラー時にファイル削除するためトランザクション開始
             DB::beginTransaction();
@@ -108,14 +110,15 @@ class UserController extends Controller
                     'name' => $request->name,
                     'account_id' => $request->account_id,
                     'profile' => $request->profile,
-                    'image_name' => '/storage/user_img/' . $filename,
+                    // 'image_name' => '/storage/user_img/' . $filename,
+                    'image_name' => '/user_img/ . $filename',
                 ]);
                 DB::commit();
 
-                // 成功時元ファイル削除
-                if ($ex_filename !== '/storage/user_img/default.jpeg') {
-                    Storage::disk('public')->delete(substr($ex_filename, 9));
-                }
+                // // 成功時元ファイル削除
+                // if ($ex_filename !== '/storage/user_img/default.jpeg') {
+                //     Storage::disk('public')->delete(substr($ex_filename, 9));
+                // }
             } catch (\Exception $e) {
                 DB::rollback();
                 // 失敗時ファイル削除
