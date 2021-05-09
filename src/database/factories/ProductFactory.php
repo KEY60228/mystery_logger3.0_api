c1799d8835beb2cdbb89af45ae124c8a9154ddec
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-05-09 19:36:08 +0900

    modify factories #13

--- src/database/factories/ProductFactory.php
+++ src/database/factories/ProductFactory.php
@@ -3,12 +3,14 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Product;
+use App\Models\Organizer;
+use App\Models\Category;
 use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        'organizer_id' => 1,
-        'category_id' => 1,
+        'organizer_id' => factory(Organizer::class)->create()->id,
+        'category_id' => factory(Category::class)->create()->id,
         'name' => $faker->word,
         'kana_name' => $faker->word,
         'phrase' => $faker->sentence,
